dbe1fb32ae6030e3a60fb5fb5194ee9447f6b711
YerinKim929 <nirey929@gmail.com>
2023-04-30 14:12:31 +0900

    [Feat] 기본 react app 및 page 생성

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
